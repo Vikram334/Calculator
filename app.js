88827bb6c6b308201c24bf433a70a81abde92832
Vikram334 <140005538+Vikram334@users.noreply.github.com>
2024-07-06 16:20:01 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+let btn=document.querySelectorAll("button");
+let input=document.getElementById('inputbox');
+let string="";
+let array=Array.from(btn);
+array.forEach(btn => {
+    btn.addEventListener("click",(e)=>{
+         if (e.target.innerHTML=='=') {
+            string=eval(string);
+            input.value=string;
+         }
+       else if(e.target.innerHTML=="AC"){
+         string="";
+         input.value=string;
+       }
+       else if(e.target.innerHTML=="DEL"){
+        string=string.substring(0,string.length-1);
+
+        input.value=string;
+      }
+         else{
+            string+=e.target.innerHTML;
+            input.value=string;
+         }
+         
+    })
+});
+
+
+
+
+
